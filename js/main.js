144ac274c10e3baaf19b3d6c1286c1709a0a7caa
scottsonnleitner <71006436+pringscreem@users.noreply.github.com>
2024-06-29 23:49:44 -0600

    First pass at making email work

--- js/main.js
+++ js/main.js
@@ -4,3 +4,16 @@ year.setAttribute("datetime", thisYear);
 year.textContent = thisYear;
 
 //He is not using semicolons.  These might need to be removed.
+
+function submitForm() {
+    let name = document.getElementById('name').value;
+    let name = document.getElementById('email').value;
+    let name = document.getElementById('message').value;
+
+    let subject = "Contact Form Submission";
+    let body = "Name: " + name + "\nEmail: " + email + "\n\nMessage:\n" + MessageChannel;
+
+    let mailtoLink = "mailto:sonnleitner.s@gmail.com" + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+
+    window.open(mailtoLink);
+}
